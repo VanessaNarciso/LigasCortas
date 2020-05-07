81b0c6282c051f5b351f99826bf5e131cbca4324
rbtote <a01366943@itesm.mx>
2020-05-06 21:11:54 -0500

    Checar getUsers de idCompany

--- dist/js/getCompanies.js
+++ dist/js/getCompanies.js
@@ -1,6 +1,10 @@
 
 
 function getCompanies() {
+    /*if(window.localStorage.tipo>0){
+        alert("No permitido entrar");
+        window.location = 'index.html';
+    }*/
     $.ajax({
         url: 'https://ligascortas.herokuapp.com/getCompanies',      
         headers: {
